8eacda9942bd4cc64e3511b092a0cfdd62cac6c0
maemon4095 <maemondecocted@gmail.com>
2023-11-12 18:13:22 +0900

    add mode to tests/build.ts

--- tests/builder/build.ts
+++ tests/builder/build.ts
@@ -1,10 +1,24 @@
 import { Builder } from "../../src/builder.ts";
 import { BuilderOptions } from "../../src/options.ts";
 
+const mode = Deno.args.at(0);
+if (mode === undefined) {
+    throw new Error("no mode was provided");
+}
+
 const options: BuilderOptions = {
     denoConfigPath: "./deno.json"
 };
 
 const builder = new Builder(options);
 
-await builder.serve();
+switch (mode) {
+    case "serve": {
+        await builder.serve();
+        break;
+    }
+    case "build": {
+        await builder.build();
+        break;
+    }
+}
